7a3f2af558b0ba7e11e24686846d9a0f71ce4794
Rajat Hebbar <rajathebbar@gmail.com>
2022-11-15 14:17:04 -0800

    Update traintest.py

--- training/traintest.py
+++ training/traintest.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time    : 6/10/21 11:00 PM
-# @Author  : Yuan Gong
-# @Affiliation  : Massachusetts Institute of Technology
-# @Email   : yuangong@mit.edu
-# @File    : traintest.py
-
 import sys
 import os
 import datetime
